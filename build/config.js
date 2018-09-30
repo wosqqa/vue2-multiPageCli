375e7420aad4718fd36a55201f5e3d0cb0e85ad1
chenzhenwei <chenzhenwei@021.com>
2018-09-30 17:45:17 +0800

    基础脚手架

---
+++ build/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  HTMLDirs: [
+    {
+      page: 'index',
+      title: '首页'
+    },
+    {
+      page: 'list',
+      title: '列表页'
+    },
+    {
+      page: 'detail',
+      title: '详情页'
+    }
+  ],
+  cssPublicPath: '../',
+  imgOutputPath: 'img/',
+  fontOutputPath: 'font',
+  cssOutputPath: './css/styles.css',
+  devServerOutputPath: '../dist',
+};
